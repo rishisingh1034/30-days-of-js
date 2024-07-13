a64cc2a319439966b4cc4731c04529d83e249ff7
Rishi Kumar Singh <rishisingh43210@gmail.com>
2024-07-13 19:55:31 +0530

    Day-01 completed

---
+++ 01_variable_data_types/index.js
@@ -0,0 +1,82 @@
+
+//activity 1
+//task 01
+
+var num = 10;
+console.log(num);
+
+//task 02
+
+let str = "Chai aur Code";
+console.log(str);
+
+//activity 2
+//task 03
+
+const opt = true;
+console.log(opt);
+
+//activity 3
+//task 04
+
+let num1 = 5;
+let str1 = "JAVASCRIPT";
+let bools = true;
+let obj = {
+    a:1,
+    b:2,
+    c:3
+}
+let arr = [1,2,3,4,5];
+console.log(typeof num1);
+console.log(typeof str1);
+console.log(typeof bools);
+console.log(typeof obj);
+console.log(typeof arr);
+
+//activity 4
+//task 05
+
+let word = "js";
+word = "javascript"
+console.log(word);
+
+//activity 5
+//task 06
+const task06 = "js";
+task06 = "ts";
+console.log(task06);
+
+
+// feature request
+
+// variable type console log
+
+let num3 =10
+ let name3 = "code";
+ let bool = true;
+ let collection =[1,2,3,"js"]
+let obj2 ={
+    user:"hello",
+    city:"world"
+}
+
+console.log(num,typeof num);
+console.log(name3,typeof name3);
+console.log(bool, typeof bool);
+console.log(collection,typeof collection);
+console.log(obj2,typeof obj2);
+
+//reassignment demo
+
+let variable1 = 44;
+variable1 = 33;
+console.log(variable1);
+
+// it can be reassigned
+
+const variable2 = 44;
+variable2 = 33;
+console.log(variable2);
+
+// cannot be reassigned
