2e5682af0082c2738e991ae02c1b962e3ef1f15f
Rishi Kumar Singh <rishisingh43210@gmail.com>
2024-07-14 10:16:13 +0530

    Add Day-02

---
+++ 02_operators/index.js
@@ -0,0 +1,80 @@
+// Tasks / Activities or Feature Request :
+
+// Activity 1: Arithmetic Operations
+
+let a = 10;
+let b = 10;
+
+// task 1
+const sum = a + b;
+console.log(sum);
+
+// task 2
+const sub = a - b;
+console.log(sub);
+
+//task3
+const mul = a * b;
+console.log(mul);
+
+//task4
+const divide = 10 / 10;
+console.log(divide);
+
+// task 5
+const remainder = a % b;
+console.log(remainder);
+
+// Activity 2: Assignment Operators
+
+// task 6
+let num = 15;
+num += 20;
+console.log(num);
+
+// task 7
+num -= 10;
+console.log(num);
+
+// Activity 3: Comparison Operators
+
+//task 8
+let numone = 9;
+let numtwo = 10;
+console.log(numone > numtwo);
+console.log(numone < numtwo);
+
+//task 9
+let num3 = 3;
+let num4 = 3;
+console.log(num3 >= num4);
+num4 = 4;
+console.log(num3 <= num4);
+
+//task 10
+let val1 = "25";
+let val2 = 25;
+console.log(val1 == val2);
+console.log(val1 === val2);
+
+
+// Activity 4: Logical Operators
+
+let var1 = 5 < 4;
+let var2 = 5 < 6;
+
+//task 11
+console.log(var1 && var2);
+
+//task 12
+console.log(var1 || var2);
+
+//task 13
+console.log(var1 || !var2);
+
+// Activity 5: Ternary Operator
+
+//task 14
+
+let Ternary = 5 > 0 ? "positive" : "negative";
+console.log(Ternary);
--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 ## This is github repository where i am doing 30 days of javascript challenge provided by (https://chaicode.com/).
 
 ### Day-01 Variables and Data Types
-### [link](./01_variable_data_types/index.js)
+### [Day-01](./01_variable_data_types/index.js)
+
+### Day-02 Operators
+### [Day-02](./02_operators/index.js)
